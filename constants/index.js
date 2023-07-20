3fcf4f5d7ecf77ad12e5e63828692d09e3f800e3
Winter <55686554+Winter1910@users.noreply.github.com>
2023-07-20 16:27:37 +0700

    Update index.js
    Update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x838df9420e810655d1c86c7f3e9a3181d0dc61c4";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
